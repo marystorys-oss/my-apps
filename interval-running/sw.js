7a4a6243529644f7fc16298944963175e3d3683d
marystorys-oss <marystorys@gmail.com>
2026-09-17 10:07:04 +0900

    Create sw.js

---
+++ interval-running/sw.js
@@ -0,0 +1,34 @@
+// 인터벌 러닝 Service Worker
+const CACHE_NAME = 'interval-running-v1';
+const CACHED_FILES = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+];
+
+// 설치: 파일 캐시
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(CACHED_FILES))
+    );
+    self.skipWaiting();
+});
+
+// 활성화: 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+// 요청: 캐시 우선, 없으면 네트워크
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => cached || fetch(event.request))
+    );
+});
